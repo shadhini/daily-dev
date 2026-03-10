c89b5c5b91141b834c2db5130e0e4955c4975be1
shadhini <shadhini.jayatilake@gmail.com>
2026-03-10 22:20:12 +0530

    linux: bash: view architecture related info

---
+++ linux/bash-commands-and-syntax/architecture-related-info.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# display information about the CPU architecture and topology on your system
+lscpu
+# Architecture:                x86_64
+#  CPU op-mode(s):            32-bit, 64-bit
+#  Address sizes:             39 bits physical, 48 bits virtual
+#  Byte Order:                Little Endian
+
+
