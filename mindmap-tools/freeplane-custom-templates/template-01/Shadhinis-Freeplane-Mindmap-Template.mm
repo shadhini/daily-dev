5aaddd597cb1613c18924c32e6faf1af23f7aa74
shadhini <shadhini.jayatilake@gmail.com>
2026-06-11 18:22:59 +0530

    add freeplane mindmap custom template

---
+++ mindmap-tools/freeplane-custom-templates/template-01/Shadhinis-Freeplane-Mindmap-Template.mm
@@ -0,0 +1,182 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="Shadhini&apos;s Freeplane Mindmap Template" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1781168810719" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#fbf1c7ff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_gruvbox_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false" newNodeStyle=":style:default"/>
+    <tags category_separator="::" tagcolor1="#Tag2#443cfeff" tagcolor2="#Tag_1#00da17ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_7974122" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt" COMMON_HGAP_QUANTITY="14 pt" CHILD_NODES_LAYOUT="AUTO">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_7974122" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" UNDERLINED="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#cccccc"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags" COLOR="#ffffff" BACKGROUND_COLOR="#ff99cc">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" BACKGROUND_COLOR="#ccffff">
+<edge STYLE="hide_edge"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#999999" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cccccc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_103960811" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="licq"/>
+<arrowlink COLOR="#ff9900" TRANSPARENCY="255" DESTINATION="ID_103960811"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#ff9933"/>
+</stylenode>
+<stylenode TEXT="Do&apos;s" COLOR="#000000" BACKGROUND_COLOR="#ccffcc" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="button_ok"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#00cc66"/>
+</stylenode>
+<stylenode TEXT="Don&apos;ts" COLOR="#ffffff" BACKGROUND_COLOR="#ff9999" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="button_cancel"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#cc241d"/>
+</stylenode>
+<stylenode TEXT="Topic Purple" COLOR="#ffffff" BACKGROUND_COLOR="#6666ff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode TEXT="Topic Pink" COLOR="#ffffff" BACKGROUND_COLOR="#ff99ff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode TEXT="Topic Green" COLOR="#ffffff" BACKGROUND_COLOR="#00cccc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode TEXT="Important Note" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_COLOR_ALPHA="0" BORDER_DASH_LIKE_EDGE="false">
+<icon BUILTIN="attach"/>
+<font SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Best Practices" COLOR="#000000" BACKGROUND_COLOR="#ccffcc" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_COLOR_ALPHA="0" BORDER_DASH_LIKE_EDGE="false">
+<icon BUILTIN="attach"/>
+<font SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Label" COLOR="#000000" BACKGROUND_COLOR="#ccffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_COLOR_ALPHA="0" BORDER_DASH_LIKE_EDGE="false">
+<icon BUILTIN="attach"/>
+<font SIZE="10" BOLD="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#6666ff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#9999ff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#cc99ff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#ff9999" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" BACKGROUND_COLOR="#ccccff" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#000000" BACKGROUND_COLOR="#ffcccc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="HEADINGS"/>
+<node TEXT="Default" POSITION="bottom_or_right" ID="ID_604674928" CREATED="1781168705790" MODIFIED="1781168712970"/>
+<node TEXT="Floating Node" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_12317824" CREATED="1781168713369" MODIFIED="1781168722835"/>
+<node TEXT="Important" LOCALIZED_STYLE_REF="styles.important" POSITION="bottom_or_right" ID="ID_1388660497" CREATED="1781168723898" MODIFIED="1781168732467"/>
+<node TEXT="Do&apos;s" STYLE_REF="Do&apos;s" POSITION="bottom_or_right" ID="ID_1910062146" CREATED="1781168732815" MODIFIED="1781168742036"/>
+<node TEXT="Don&apos;ts" STYLE_REF="Don&apos;ts" POSITION="bottom_or_right" ID="ID_1777052152" CREATED="1781168742428" MODIFIED="1781168750860"/>
+<node TEXT="Topic Purple" STYLE_REF="Topic Purple" POSITION="bottom_or_right" ID="ID_1121851464" CREATED="1781168751190" MODIFIED="1781168758181">
+<node TEXT="Level 2" ID="ID_1914464135" CREATED="1781168795447" MODIFIED="1781168830815">
+<node TEXT="Level 3" ID="ID_707123934" CREATED="1781168815250" MODIFIED="1781168836497">
+<node TEXT="Level 4" ID="ID_1028113221" CREATED="1781168836500" MODIFIED="1781168844092">
+<node TEXT="Level 5" ID="ID_1926549799" CREATED="1781168844096" MODIFIED="1781168850691">
+<node TEXT="Level 6" ID="ID_1163027882" CREATED="1781168850694" MODIFIED="1781168854011">
+<node TEXT="Level 7" ID="ID_1530747654" CREATED="1781168854015" MODIFIED="1781168857555">
+<node TEXT="Leaf" ID="ID_1293208842" CREATED="1781168862399" MODIFIED="1781168873219"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Topic Green" STYLE_REF="Topic Green" POSITION="bottom_or_right" ID="ID_320916021" CREATED="1781168758600" MODIFIED="1781168766865"/>
+<node TEXT="Topic Pink" STYLE_REF="Topic Pink" POSITION="bottom_or_right" ID="ID_1468022706" CREATED="1781168767304" MODIFIED="1781168776927"/>
+<node TEXT="Important Note" STYLE_REF="Important Note" POSITION="bottom_or_right" ID="ID_218137547" CREATED="1781179450067" MODIFIED="1781179466020"/>
+<node TEXT="Best Practices&#xa;- practice 1&#xa;- practice 2" STYLE_REF="Best Practices" POSITION="bottom_or_right" ID="ID_996559681" CREATED="1781179466442" MODIFIED="1781179502531"/>
+<node TEXT="Label" STYLE_REF="Label" POSITION="bottom_or_right" ID="ID_689267275" CREATED="1781179507610" MODIFIED="1781179515109"/>
+<node TEXT="Node with Details" LOCALIZED_STYLE_REF="default" POSITION="bottom_or_right" ID="ID_937450945" CREATED="1781170709680" MODIFIED="1781170754131"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Node Details Here
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Node with Note" LOCALIZED_STYLE_REF="default" POSITION="bottom_or_right" ID="ID_444861692" CREATED="1781170779674" MODIFIED="1781171214820"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This is node Note
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Node with Tags" POSITION="bottom_or_right" ID="ID_426407791" CREATED="1781168777277" MODIFIED="1781168963430" TAGS="#Tag_1&#xa;#Tag2"/>
+<node TEXT="Node with Attributes" POSITION="bottom_or_right" ID="ID_666539353" CREATED="1781168970085" MODIFIED="1781169168564" TAGS="#Tag2">
+<attribute NAME="Attribute_1" VALUE="attr1-value"/>
+<attribute NAME="Attribute_2" VALUE="attr2-value"/>
+</node>
+<node TEXT="Node with Details, Note, Tags and Attributes" LOCALIZED_STYLE_REF="default" POSITION="bottom_or_right" ID="ID_1724223388" CREATED="1781171320532" MODIFIED="1781171410966" TAGS="#Tag2&#xa;#Tag_1"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <div>
+      <div align="left" style="background-color: #ffffcc; width: 500pt">
+        <p>
+          This is node Note
+        </p>
+      </div>
+    </div>
+  </body>
+</html></richcontent>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Node details here...
+    </p>
+  </body>
+</html></richcontent>
+<attribute NAME="Attribute 1" VALUE="attr1"/>
+<attribute NAME="Attribute 2" VALUE="attr2"/>
+</node>
+</node>
+</map>
---
+++ mindmap-tools/freeplane.md
@@ -0,0 +1,20 @@
+# Freeplane
+
+## Create a template for mind maps
+
+- `File` > `New map from template...` > Select the base template you want to use
+- `Format` > `Manage Styles` > `Edit Styles` 
+- Now you can customize the styles
+
+## Add node details
+
+Right-click on a node > `Edit` > `Edit node details in dialog` OR `Edit node details in-line`
+
+## Add a note to a node
+
+Right-click on a node > `Edit` > `Edit note in dialog`
+
+## Turn off automatic node collapsing
+
+`Freeplane` > `Settings...` > `Behaviour` > Uncheck `Fold on click inside`
+
